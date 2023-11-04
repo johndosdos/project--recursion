0d7e891a10d420a7d86448735ab48a51dc8d86d0
John <128350391+alpaOo@users.noreply.github.com>
2023-11-04 09:32:54 +0800

    Add solution to the problem using dynamic programming

--- assignment 1: fibonacci sequence/fibonacciSequence.js
+++ assignment 1: fibonacci sequence/fibonacciSequence.js
@@ -1,6 +1,8 @@
 //fibonacci sequence, two approach
 //recursion and dynamic programming (bottom-up approach)
 
+//we'll use the 41st fibonacci number as an example
+
 //recursion
 function fibonacci(n) {
   try {
@@ -18,3 +20,26 @@ function fibonacci(n) {
   }
 }
 console.log(fibonacci(41));
+
+/* //dynamic programming (bottom-up approach)
+function fibonacci(n, memo = [0, 1, 1]) {
+  try {
+    if (n < 0) {
+      throw new Error("Out of range");
+    } else if (memo[n] !== undefined) {
+      return memo[n];
+    } else if (n === 0) {
+      return memo[n];
+    } else if (n <= 2) {
+      return memo[n];
+    } else {
+      for (let i = 3; i <= n; i += 1) {
+        memo[i] = memo[i - 1] + memo[i - 2];
+      }
+      return memo[n];
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+console.log(fibonacci(41)); */
